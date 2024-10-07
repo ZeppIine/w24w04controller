92e4ceac40c63a0914fde51eac8feca8fcd7e5c1
ZeppIine <kim0904min@gmail.com>
2024-10-07 16:05:27 +0900

    change data

--- src/main/kotlin/kr/ac/kumoh/ce/s20180147/w24w04controller/repository/SongRepository.kt
+++ src/main/kotlin/kr/ac/kumoh/ce/s20180147/w24w04controller/repository/SongRepository.kt
@@ -5,11 +5,13 @@ import org.springframework.stereotype.Repository
 
 @Repository
 class SongRepository {
-  val songs = listOf(
+  private val songs = listOf(
     Song(1, "夏が僕らを呼ぶのなら ( If Summer is calling us) feat.わかばやし", "*Luna"),
     Song(2, "Rise up feat.初音ミク", "*Luna"),
     Song(3, "あの夏のいつかは (Someday in that Summer) feat.ねんね | 2023 Ver.", "*Luna"),
   )
 
   fun fetchSong() = songs
+
+  fun getSong(index: Int) = songs[index]
 }
--- src/main/kotlin/kr/ac/kumoh/ce/s20180147/w24w04controller/service/SongService.kt
+++ src/main/kotlin/kr/ac/kumoh/ce/s20180147/w24w04controller/service/SongService.kt
@@ -3,10 +3,15 @@ package kr.ac.kumoh.ce.s20180147.w24w04controller.service
 import kr.ac.kumoh.ce.s20180147.w24w04controller.model.Song
 import kr.ac.kumoh.ce.s20180147.w24w04controller.repository.SongRepository
 import org.springframework.stereotype.Service
+import kotlin.random.Random
 
 @Service
 class SongService(val repository: SongRepository) {
   fun getAllSongs(): List<Song> {
     return repository.fetchSong()
+  }
+
+  fun getRandomSong(): Song {
+    return repository.getSong(Random.nextInt(3))
   }
 }
